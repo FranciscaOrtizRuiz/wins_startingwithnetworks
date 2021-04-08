728ce4cf1a6bfbba3b7771b99a5951db58cd9dc5
FranciscaOrtizRuiz <franortizruiz@gmail.com>
2021-04-08 23:38:05 +0100

    add change image

--- imag/sticker.R
+++ imag/sticker.R
@@ -3,7 +3,7 @@
 
 library(hexSticker)
 
-g<-"imag/log.jpg"
+g<-"imag/log.png"
 
 sticker(g,package="",
         p_size=3.0, #size text
@@ -12,14 +12,14 @@ sticker(g,package="",
         p_color = "black", # colour text
         p_family = "gloria", #Aller_Rg
         #spotlight=TRUE,
-        #u_size=0.9,
+        u_size=3.9,
         s_x=1.0,  # x centering
         s_y=1.0, # y centering
         s_width=0.5,
         s_height=0.5,
-        h_fill="#FFFFFF", 
-        h_color="#A9A9A9", filename="imag/sticker.jpg", 
-        asp = 1.0, dpi = 1000
+        h_fill="#eab6ab", 
+        h_color="#8c7faa", filename="imag/sticker.jpg", 
+        asp = 1.2, dpi = 1000
 )
 
 
